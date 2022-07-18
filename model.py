c2b5e8e12078b193868ae65735ab7a92c846aac4
takeru1205 <takeru0639418@gmail.com>
2022-07-19 00:01:45 +0900

    [update] teacher model

--- model.py
+++ model.py
@@ -17,3 +17,18 @@ class model(nn.Module):
 
         return output
 
+
+class teacher_model(nn.Module):
+    def __init__(self, input_size, output_size, hidden_size):
+        super(model, self).__init__()
+        self.rnn = nn.LSTM(input_size = input_size,
+                           hidden_size = hidden_size,
+                           batch_first = True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, inputs):
+        h, _= self.rnn(inputs)
+        output = nn.Sigmoid(self.fc(h[:, -1]))
+
+        return output
+
